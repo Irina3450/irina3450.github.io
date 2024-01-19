ff2915fa906a160bbbddb4c1b625c3f66e869429
Irina3450 <zaregorodzeva345@gmail.com>
2024-01-19 10:54:42 +0300

    change

--- tehno/js/main.js
+++ tehno/js/main.js
@@ -1,11 +1,17 @@
+// Шапрка сайта
 $(function(){
   $('.header-menu-bth').click(function(){
     if($(this).hasClass('active')){
         $(this).removeClass('active');
-        $('.container-header-minu').removeClass('open');
+        $('.container-header-minu, .overwlay').removeClass('open');
+        
     } else {
         $(this).addClass('active');
-        $('.container-header-minu').addClass('open');
+        $('.container-header-minu, .overwlay').addClass('open');
     }
   })  
+  $('.overwlay.open').click(function(){
+    $(this).removeClass('open');
+    $('.container-header-minu').removeClass('open');
+  })
 });
